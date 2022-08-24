cf83b34620dbef28869b6fda61bd97bd1bde7f70
claireeeeey <90796587+claireeeeey@users.noreply.github.com>
2022-08-24 02:07:04 -0500

    Update and rename index.js to front page/index.js

--- front page/index.js
+++ front page/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav_toggle');
 
+//add a click listener to the navigation bar to open the menu
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
